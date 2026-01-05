6e3136180f4303302f8aff64849280c39e06d07e
KvM <k044269725@gmail.com>
2026-01-05 20:00:21 +0100

    Setting GitHub

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// import './style.css'
+
+const main = () => {
+	prepareDOMElements();
+	prepareDOMEvents();
+};
+
+// Load DOME content
+
+const prepareDOMElements = () => {};
+
+// Load all events
+
+const prepareDOMEvents = () => {};
+
+// Load main function on Web load
+
+document.addEventListener("DOMContentLoaded", () => {
+	main();
+});
