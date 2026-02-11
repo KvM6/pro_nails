af04bc9a99e60dd657180e9bc082556f4174ea6d
KvM <k044269725@gmail.com>
2026-02-11 21:57:19 +0100

    Testing deploying Vite project to GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+// 	plugins: [tailwindcss()],
+// 	base: "/pro_nails/",
+// 	resolve: {
+// 		alias: {
+// 			"@": path.resolve(__dirname, "src"),
+// 		},
+// 	},
+// });
+
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import tailwindcss from "@tailwindcss/vite";
 
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
-	plugins: [tailwindcss()],
-});
+		plugins: [tailwindcss()],
+  base: '/portfolioPageTwo/', // your repo name
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+})
