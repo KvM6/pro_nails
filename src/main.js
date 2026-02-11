7d6dfda25ec879835698e9a70daa98789193bd18
KvM <k044269725@gmail.com>
2026-02-11 20:06:30 +0100

    Addes some js for simple work, little design fixes.

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import "./style.css";
 
 // Navigation
 
+let brandBtn;
 let hamburger;
 let navMobile;
+let navMobileLinks;
+let navDesk;
+let navDeskLinks;
+
+// Header
+
+let headBtn;
+let aboutSection;
 
 const main = () => {
 	prepareDOMElements();
@@ -16,17 +25,54 @@ const main = () => {
 // Load DOME content
 
 const prepareDOMElements = () => {
+	// Navigation
+
+	brandBtn = document.querySelector(".nav-brand");
 	hamburger = document.querySelector(".hamburger");
 	navMobile = document.querySelector(".nav-mobile");
+	navMobileLinks = document.querySelectorAll(".mobile-item");
+	navDesk = document.querySelector(".nav-desktop");
+	navDeskLinks = document.querySelectorAll(".desktop-items");
+
+	// Header
+
+	headBtn = document.querySelector(".header-button");
+	aboutSection = document.querySelector("#about");
 };
 
 // Load all events
 
 const prepareDOMEvents = () => {
+	// Navigation
+
+	brandBtn.addEventListener("click", () => {});
+
 	hamburger.addEventListener("click", () => {
 		hamburger.classList.toggle("is-active");
 		navMobile.classList.toggle("translate-y-full");
 		navMobile.classList.toggle("translate-none");
+	});
+
+	navMobileLinks.forEach((item) => {
+		item.addEventListener("click", () => {
+			setTimeout(() => {
+				hamburger.classList.remove("is-active");
+				navMobile.classList.toggle("translate-y-full");
+				navMobile.classList.toggle("translate-none");
+			}, 500);
+		});
+	});
+
+	// Header
+
+	headBtn.addEventListener("click", () => {
+		if (aboutSection) {
+			aboutSection.scrollIntoView({
+				behavior: "smooth",
+				block: "start",
+			});
+			console.log("work");
+		}
 	});
 };
 
